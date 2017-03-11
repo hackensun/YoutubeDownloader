e2b7130fb86c319b9e94b1b20e8a8b18e1615986
Khoa Pham <onmyway133@gmail.com>
2017-03-11 18:41:52 +0100

    Add TaskDelegate

--- YoutubeDownloader/Utils/Task.swift
+++ YoutubeDownloader/Utils/Task.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
-struct Task {
+protocol TaskDelegate: class {
+  func task(task: Task, didOutput string: String)
+}
+
+class Task {
+
+  weak var delegate: TaskDelegate?
 
   func run(arguments: [String]) {
     let launchPath = Bundle.main.path(forResource: "youtube-dl", ofType: "")!
@@ -25,12 +31,17 @@ struct Task {
     let stdErr = Pipe()
     process.standardError = stdErr
 
-    let handler =  { (file: FileHandle!) -> Void in
+    let handler =  { [weak self] (file: FileHandle!) -> Void in
       let data = file.availableData
       guard let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
         else { return}
 
-      print(output.components(separatedBy: "\n").first!)
+      guard let strongSelf = self,
+        let string = output.components(separatedBy: "\n").first else {
+        return
+      }
+
+      strongSelf.delegate?.task(task: strongSelf, didOutput: string)
     }
 
     stdErr.fileHandleForReading.readabilityHandler = handler
